9d61f05639dbe82c3e0ddaf2964b3ffcf249fd36
Lucas Moreno Silva <lucasmorenosilva@gmail.com>
2016-03-24 21:14:16 -0300

    Add default scope and more data validation in manufacturer model

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,4 +1,8 @@
 class Manufacturer < ActiveRecord::Base
   has_many :models
+
   validates_presence_of :name, :webmotors_id
+  validates_uniqueness_of :name, :webmotors_id
+
+  default_scope { order :name }
 end
