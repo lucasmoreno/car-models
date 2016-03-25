41d84acfecc76aba364914ab22ed5ba8c7a45ec2
Lucas Moreno Silva <lucasmorenosilva@gmail.com>
2016-03-24 23:52:02 -0300

    Remove unnecessary find_by_id

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -1,8 +1,7 @@
 class ModelsController < ApplicationController
   def index
-    if manufacturer = Manufacturer.find_by_id(params[:manufacturer_id])
-      WebmotorsService.fetch_models_from_manufacturer manufacturer
-      @manufacturer = Manufacturer.find_by_id params[:manufacturer_id]
+    if @manufacturer = Manufacturer.find_by_id(params[:manufacturer_id])
+      WebmotorsService.fetch_models_from_manufacturer @manufacturer
     else
       redirect_to root_path
     end
