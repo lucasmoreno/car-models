0a20530859d44ee84b74a4f8557ee42a89037f1a
Lucas Moreno Silva <lucasmorenosilva@gmail.com>
2016-03-24 22:15:44 -0300

    Add partial view for model and refactor index

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -4,15 +4,15 @@ class ModelsController < ApplicationController
     uri = URI("http://www.webmotors.com.br/carro/modelos")
 
     # Make request for Webmotors site
-    manufacturer = Manufacturer.where(webmotors_id: params[:webmotors_manufacturer_id])[0]
+    @manufacturer = Manufacturer.find_by_id params[:manufacturer_id]
 
     response = Net::HTTP.post_form(uri, { marca: params[:webmotors_manufacturer_id] })
     models_json = JSON.parse response.body
 
     # Itera no resultado e grava os modelos que ainda não estão persistidas
     models_json.each do |json|
-      if Model.where(name: json["Nome"], manufacturer_id: manufacturer.id).size == 0
-        Model.create(manufacturer_id: manufacturer.id, name: json["Nome"])
+      if Model.where(name: json["Nome"], manufacturer_id: @manufacturer.id).size == 0
+        Model.create(manufacturer_id: @manufacturer.id, name: json["Nome"])
       end
     end
   end
